736051049f89e43869dc67b9229f224717695d81
Andreas Plüss <andreaspluess@gmail.com>
2014-03-01 11:04:25 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trololo::Application.config.secret_key_base = '985beac9758c1e0998193112e2f0ca7881e2a46bd4a238b108f2e52d30bacf1086d0e6958c3e3269fcb54fe9452a6a9bf24d7386e33f7fd6a45c0874e0969a7c'
