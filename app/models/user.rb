f882c67288ef1dbd730cb89e092ab8d3a7421098
Raffael Schmid <raffael@yux.ch>
2014-03-15 11:38:26 +0100

    add missing workshop/user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :owned_workshops, class_name: 'Workshop', foreign_key: 'owner_id'
   has_many :leaded_groups,   class_name: 'Group',    foreign_key: 'leader_id'
 
+  has_many :attendances
+  has_many :workshops, through: :attendances
+
   has_many :memberships
   has_many :groups, through: :memberships
 
