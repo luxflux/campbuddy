bdaf182372eeadd489168abcdd5f34b171f7cb7e
Raffael Schmid <raffael@yux.ch>
2014-03-15 11:28:15 +0100

    add the relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,7 @@
 class Group < ActiveRecord::Base
-  belongs_to :leader
+  belongs_to :leader, class_name: 'User'
+
+  has_many :memberships
+  has_many :users, through: :memberships
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :owned_workshops, class_name: 'Workshop', foreign_key: 'owner_id'
+  has_many :leaded_groups,   class_name: 'Group',    foreign_key: 'leader_id'
+
+  has_many :memberships
+  has_many :groups, through: :memberships
 
   validates :email, presence: true
 end
