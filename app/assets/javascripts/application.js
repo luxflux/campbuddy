8b227dad9fb325e2482b7c8910968f666943d158
Andreas Plüss <andreaspluess@gmail.com>
2014-04-12 13:08:48 +0200

    vendor-Javascript und Stylesheets aus Gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery.min
+//= require jquery
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
