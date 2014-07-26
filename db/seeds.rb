e9af5844896c583fa70f0090d87dea0d5f3f315d
Raffael Schmid <raffael@yux.ch>
2014-07-26 13:53:35 +0200

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.where(email: 'admin@example.org')
+if admin.first
+  puts 'Admin already existed with email admin@example.org'
+else
+  admin.first_or_create!(firstname: 'Mr', name: 'Admin', password: 'adminpass', admin: true)
+  puts 'Created admin: admin@example.org/adminpass'
+end
