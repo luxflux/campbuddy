c31de1e1e2191d1ab065294722f97664bca823da
Raffael Schmid <raffael@yux.ch>
2014-10-11 14:14:41 +0200

    add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ActiveRecord::Base
   belongs_to :category
   has_many :attendances
   has_many :users, through: :attendances
+
+  validate :owner, presence: true
+  validate :category, presence: true
+  validate :title, presence: true
+  validate :description, presence: true
+  validate :starts, presence: true
+  validate :ends, presence: true
+
 end
