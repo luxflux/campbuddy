dcb9c5d3171f876c1ef47674479d29c8c5be1f1a
Andreas Plüss <andreaspluess@gmail.com>
2014-10-18 13:00:26 +0200

    user design and events logic within user view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home' => 'home#index'
+
   resources :jobs
 
   resources :memberships
@@ -18,7 +20,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'workshops#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
