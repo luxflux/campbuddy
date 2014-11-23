4e3783405745a661b275c75b5acf86ae627bacb4
Raffael Schmid <raffael@yux.ch>
2014-11-23 20:01:47 +0100

    Translate rails admin

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Trololo
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.available_locales = [:de]
+    config.i18n.available_locales = [:de, :en]
     config.i18n.default_locale = :de
   end
 end
