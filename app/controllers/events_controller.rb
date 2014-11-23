3005f3c85a41f89d3df9dcc7f542558d8bcdac02
Raffael Schmid <raffael@yux.ch>
2014-11-23 20:08:16 +0100

    Creation and Removal of events is handled via rails admin

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,24 +16,8 @@ class EventsController < ApplicationController
   def show
   end
 
-  # GET /events/new
-  def new
-    @event = Event.new
-  end
-
   # GET /events/1/edit
   def edit
-  end
-
-  # POST /events
-  def create
-    @event = Event.new(event_params)
-
-    if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
-    else
-      render action: 'new'
-    end
   end
 
   # PATCH/PUT /events/1
@@ -43,12 +27,6 @@ class EventsController < ApplicationController
     else
       render action: 'edit'
     end
-  end
-
-  # DELETE /events/1
-  def destroy
-    @event.destroy
-    redirect_to events_url, notice: 'Event was successfully destroyed.'
   end
 
   private
