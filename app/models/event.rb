6992eee1279150db2d28d0d491cbcb643778a398
Raffael Schmid <raffael@yux.ch>
2014-11-23 18:41:05 +0100

    add tests for max attendees logic, refs #34

--- app/models/event.rb
+++ app/models/event.rb
@@ -29,4 +29,16 @@ class Event < ActiveRecord::Base
     return nil unless max_attendees
     max_attendees - users.count
   end
+
+  def any_places_left?
+    attendance_places_left.nil? || attendance_places_left > 0
+  end
+
+  def no_places_left?
+    !any_places_left?
+  end
+
+  def attended_by_user?(user)
+    users.include? user
+  end
 end
