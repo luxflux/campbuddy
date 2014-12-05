e2395e5de68f355a0a475275ed8346120b635dab
Raffael Schmid <raffael@yux.ch>
2014-12-05 08:54:16 +0100

    some more translations for Event and make description a text field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126222415) do
+ActiveRecord::Schema.define(version: 20141205075254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20141126222415) do
   create_table "events", force: true do |t|
     t.integer  "owner_id"
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.string   "meeting_point"
     t.datetime "starts"
     t.datetime "ends"
