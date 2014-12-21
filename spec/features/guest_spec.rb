4770451a448e91ec3b959b9a8ef90c755bc03aab
Raffael Schmid <raffael@yux.ch>
2014-12-21 23:44:35 +0100

    Modify Event detail view for guest users, refs #52

---
+++ spec/features/guest_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'Guest View' do
+  background do
+    visit root_url
+    click_link 'Als Gast einloggen'
+  end
+
+  let(:event) { FactoryGirl.create :event }
+
+  scenario 'allows listing events catalog' do
+    event.touch
+    visit catalog_events_url
+    expect(page).to have_content event.title
+  end
+
+  describe 'event details' do
+    background do
+      visit event_path(event)
+    end
+
+    scenario 'allows opening an event detail view' do
+      expect(page).to have_content event.title
+    end
+
+    scenario 'it hides the attendance bar' do
+      expect(page).to_not have_css('.checked')
+      expect(page).to_not have_css('.unchecked')
+    end
+
+    scenario 'it hides the attendees' do
+      expect(page).to_not have_css('.partipicants')
+    end
+
+    scenario 'it hides the owner' do
+      expect(page).to_not have_css('li.owner')
+    end
+  end
+
+end
