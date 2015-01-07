833aa3807973c64479a156ea6ee3bc3a3af160fe
Raffael Schmid <raffael@yux.ch>
2015-01-07 01:04:18 +0100

    use smtp instead of sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Use sendmail for mailings
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'campplaner.yux.ch' }
-
   config.action_mailer.smtp_settings = {
     address: 'smtp.mandrillapp.com',
     port: 25,
