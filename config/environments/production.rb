ffa86b6338ad5ac49fef7c2383260e900ed309db
Raffael Schmid <raffael@yux.ch>
2015-01-07 19:18:12 +0100

    use new domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # Use sendmail for mailings
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'campplaner.yux.ch' }
+  config.action_mailer.default_url_options = { host: 'campplaner.oneyouth.ch' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.mandrillapp.com',
     port: 25,
