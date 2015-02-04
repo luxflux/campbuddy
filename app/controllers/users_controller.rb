b2bc0cbaf532bf5d7120edcd7f689b6853921984
Raffael Schmid <raffael@yux.ch>
2015-02-04 23:51:35 +0100

    Revert "started to solve group mandatory events in profile"
    This reverts commit 8614d8b25bdf58d27c09add8f51637c0718bbea6.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    #@events = @user.events.in_future
-    @events = @user.events.in_future.without_group_events
-=begin
-    #filter out gruop events where you don't take part
-    @events.each do |event|
-      if (event.groups_only == true)
-        event.groups.each do |group|
-          if (group.leader == current_user) || (group.users.where(email: current_user.email))
-            delete(event)
-          end
-        end
-      end
-    end
-=end
+    @events = @user.events.in_future
   end
 
   # GET /users/1/edit
