d953834a810e044665a0afb77ad180fa67f279c6
Raffael Schmid <raffael@yux.ch>
2015-05-16 18:41:01 +0200

    fix tests again

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -5,29 +5,36 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
 
     # feature tests
-    Apartment::Tenant.drop('127_0_0_1') rescue nil
-    orga = Organization.create! name: 'default', domain: '0.0.1'
-    Camp.create! name: 'default', subdomain: '127', organization: orga
-
     Apartment::Tenant.drop('www_example_com') rescue nil
     orga = Organization.create! name: 'default2', domain: 'example.com'
     Camp.create! name: 'default2', subdomain: 'www', organization: orga
+  end
 
-    Apartment::Tenant.switch!('www_example_com')
+  config.around(:each) do |block|
+    Apartment::Tenant.switch('www_example_com') do
+      block.call
+    end
   end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
   config.around(:each, js: true) do |block|
+    DatabaseCleaner.strategy = :truncation
+
+    Apartment::Tenant.drop('127_0_0_1') rescue nil
+    orga = Organization.create! name: 'default', domain: '0.0.1'
+    Camp.create! name: 'default', subdomain: '127', organization: orga
+
     Apartment::Tenant.switch('127_0_0_1') do
       block.call
     end
+
+    # tables have been emptied as we used :truncation
+    Apartment::Tenant.drop('www_example_com') rescue nil
+    orga = Organization.create! name: 'default2', domain: 'example.com'
+    Camp.create! name: 'default2', subdomain: 'www', organization: orga
   end
 
   config.before(:each) do
