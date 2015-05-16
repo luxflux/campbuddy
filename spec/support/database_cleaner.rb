dbac29a6ab283e566296c1152d317feb7b8c3d08
Raffael Schmid <raffael@yux.ch>
2015-05-16 12:29:17 +0200

    fix the tests

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,6 +3,11 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+
+    # feature tests
+    Apartment::Tenant.drop('127_0_0_1') rescue nil
+    orga = Organization.create! name: 'default', domain: '0.0.1'
+    Camp.create! name: 'default', subdomain: '127', organization: orga
   end
 
   config.before(:each) do
@@ -11,6 +16,12 @@ RSpec.configure do |config|
 
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each, js: true) do |block|
+    Apartment::Tenant.switch('127_0_0_1') do
+      block.call
+    end
   end
 
   config.before(:each) do
