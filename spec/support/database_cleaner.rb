37b5875a84bf565f0c154af55980f1aa40f431ff
Raffael Schmid <raffael@yux.ch>
2015-05-21 23:30:58 +0200

    fix the specs, once more

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -5,13 +5,13 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
 
     # feature tests
-    Apartment::Tenant.drop('www_example_com') rescue nil
-    orga = Organization.create! name: 'default2', domain: 'example.com'
-    Camp.create! name: 'default2', subdomain: 'www', organization: orga
+    Apartment::Tenant.drop('127_0_0_1') rescue nil
+    orga = Organization.create! name: 'default2', domain: '0.0.1'
+    Camp.create! name: 'default2', subdomain: '127', organization: orga
   end
 
   config.around(:each) do |block|
-    Apartment::Tenant.switch('www_example_com') do
+    Apartment::Tenant.switch('127_0_0_1') do
       block.call
     end
   end
@@ -24,32 +24,24 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :truncation
   end
 
-  config.around(:each, js: true) do |block|
-    DatabaseCleaner.strategy = :truncation
-
-    Apartment::Tenant.drop('127_0_0_1') rescue nil
-    orga = Organization.create! name: 'default', domain: '0.0.1'
-    Camp.create! name: 'default', subdomain: '127', organization: orga
-
-    Apartment::Tenant.switch('127_0_0_1') do
-      block.call
-    end
+  config.before(:each, type: :feature) do
+    port = 3002
+    host = '127.0.0.1'
+    default_url_options[:host] = host
+    Capybara.app_host = "http://#{host}:#{port}"
+    Capybara.server_port = port
   end
 
   config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.after(:each) do |example|
     DatabaseCleaner.clean
-  end
-
-  config.after(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-
-    # tables have been emptied as we used :truncation
-    Apartment::Tenant.drop('www_example_com') rescue nil
-    orga = Organization.create! name: 'default2', domain: 'example.com'
-    Camp.create! name: 'default2', subdomain: 'www', organization: orga
+    if example.metadata[:js]
+      Apartment::Tenant.drop('127_0_0_1') rescue nil
+      orga = Organization.create! name: 'default2', domain: '0.0.1'
+      Camp.create! name: 'default2', subdomain: '127', organization: orga
+    end
   end
 end
