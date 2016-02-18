97504de21df2de1492045fa41712b4f8421b83a3
Raffael Schmid <raffael@yux.ch>
2016-02-18 15:53:16 +0100

    use rollbar access token from env

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,16 +13,6 @@ set :keep_releases, 5
 set :rbenv_type, :system
 set :rbenv_ruby, '2.2.2'
 
-task :notify_rollbar do
-  on roles(:app) do |h|
-    revision = `git log -n 1 --pretty=format:"%H"`
-    local_user = `whoami`
-    rollbar_token = 'e61ee5cf47784faa84a7deb14bb6385c'
-    rails_env = fetch(:rails_env, 'production')
-    execute "curl -s -o /dev/null https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user}", :once => true
-  end
-end
-
 namespace :deploy do
   task :restart do
     on roles(:web), in: :sequence, wait: 5 do
@@ -32,4 +22,3 @@ namespace :deploy do
 end
 
 after 'deploy:publishing', 'deploy:restart'
-after 'deploy:restart', 'notify_rollbar'
--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,8 +1,8 @@
 require 'rollbar/rails'
 
 Rollbar.configure do |config|
-  config.access_token = 'e61ee5cf47784faa84a7deb14bb6385c'
-  config.enabled = Rails.env.production? || Rails.env.staging?
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.enabled = ENV['ROLLBAR_ACCESS_TOKEN'].present?
   config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
   config.environment = ENV['ROLLBAR_ENV'] || Rails.env
 end
