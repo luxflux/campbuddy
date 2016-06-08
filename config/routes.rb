f50f85d89bce341993e01d62d69c08a68ed4c207
Raffael Schmid <raffael@yux.ch>
2016-06-08 23:10:32 +0200

    magic let's encrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  # START automagic let's encrypt stuff
+  if ENV['ACME_KEY'] && ENV['ACME_TOKEN']
+    get ".well-known/acme-challenge/#{ENV['ACME_TOKEN']}" => proc { [200, {}, [ ENV["ACME_KEY"] ] ] }
+  else
+    ENV.each do |var, _|
+      next unless var.start_with?('ACME_TOKEN_')
+      number = var.sub(/ACME_TOKEN_/, '')
+      get ".well-known/acme-challenge/#{ENV["ACME_TOKEN_#{number}"]}" => proc { [200, {}, [ ENV["ACME_KEY_#{number}"] ] ] }
+    end
+  end
+  # END automagic let's encrypt stuff
+
   resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
   resource :session, controller: 'sessions', only: [:create] do
     collection do
