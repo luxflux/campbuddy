caabfa8374a28dd7c90334e0ca0ce4e811224508
Raffael Schmid <raffael@yux.ch>
2018-01-25 21:43:55 +0100

    Remove Sabayon

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'csv'
-require File.expand_path('../../lib/sabayon_middleware', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,7 +25,5 @@ module CampPlaner
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.middleware.insert_before 0, SabayonMiddleware
   end
 end
